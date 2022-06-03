d08c7077b3b1a805506093cc1ebc5d2bc3a91095
TildaDares <mathildaudufo@gmail.com>
2022-06-03 14:27:28 +0100

    currently searches for internships worldwide

---
+++ index.js
@@ -0,0 +1,51 @@
+const PORT = 3000;
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+
+const app = express();
+
+app.get("/results", (req, res) => {
+  const internships = [];
+
+  for (let pageNumber = 0; pageNumber < 1000; pageNumber += 25) {
+    const url = `https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=internship&location=Worldwide&&trk=public_jobs_jobs-search-bar_search-submit&currentJobId=2931031787&position=1&pageNum=0&start=${pageNumber}`;
+
+    axios(url)
+      .then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const jobs = $("li");
+
+        jobs.each((index, element) => {
+          const title = $(element)
+            .find("h3.base-search-card__title")
+            .text()
+            .trim();
+          const company = $(element)
+            .find("h4.base-search-card__subtitle")
+            .text()
+            .trim();
+
+          const location = $(element)
+            .find("span.job-search-card__location")
+            .text()
+            .trim();
+
+          const link = $(element).find("a.base-card__full-link").attr("href");
+
+          internships.push({
+            title,
+            company,
+            location,
+            link,
+          });
+        });
+
+        res.json(internships);
+      })
+      .catch((err) => console.log(err));
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
